c88adff9643686f1df2d6c22501a94a206400049
Drakshayani-B-K <drakshayani.bk95@gmail.com>
2025-12-05 18:17:14 +0530

    commit1

---
+++ src/test/java/SampleTest/Demosample.java
@@ -0,0 +1,10 @@
+package SampleTest;
+
+import org.testng.annotations.Test;
+
+public class Demosample {  
+	@Test
+	public void Createdemo() {
+		System.out.println("createdemo is successfully created");
+	}
+}
---
+++ src/test/java/SampleTest/Testsample.java
@@ -0,0 +1,10 @@
+package SampleTest;
+
+import org.testng.annotations.Test;
+
+public class Testsample {
+@Test
+public void Createtest() {
+	System.out.println("Createtest is successfully created");
+}
+}
